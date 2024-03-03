670feb3737428aff8456a1655df8fb9298e0068f
ArtemSolovev1 <artem1solovev@gmail.com>
2024-03-03 09:43:04 +0300

    Some header layout changes

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,10 @@
-<script setup></script>
+<script setup>
+import Header from './components/Header.vue'
+</script>
 
 <template>
   <div class="bg-white w-4/5 mx-auto h-screen rounded-xl shadow-xl mt-14">
-    <header class="flex justify-between border-b border-slate-300 px-10 py-8">
-      <div class="flex items-center gap-4">
-        <img src="/logo.png" alt="Logo" class="w-10" />
-        <div>
-          <h2 class="text-xl font-bold uppercase">Skeakershop</h2>
-          <p class="text-slate-400">Магазин лучших кроссовок</p>
-        </div>
-      </div>
-
-      <ul class="flex items-center gap-10">
-        <li class="flex items-center gap-3 text-slate-500">
-          <img src="/cart.svg" alt="Cart" />
-          <b>1205 руб.</b>
-        </li>
-
-        <li class="flex items-center gap-3 text-slate-500">
-          <img src="/heart.svg" alt="Cart" />
-          <b>Закладки</b>
-        </li>
-
-        <li class="flex items-center gap-3 text-slate-500">
-          <img src="/profile.svg" alt="Cart" />
-          <b>Профиль</b>
-        </li>
-      </ul>
-    </header>
+    <Header />
     <h1>Blog</h1>
   </div>
 </template>
--- src/components/MyHello.vue
+++
@@ -1,3 +0,0 @@
-<script setup></script>
-
-<template><h1>Blog</h1></template>
