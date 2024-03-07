3d59419e2022afb7de965df8222b93fbb534db02
ArtemSolovev1 <artem1solovev@gmail.com>
2024-03-07 21:11:02 +0300

    Seach and filter buttons layouts

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,30 @@ import Drawer from './components/Drawer.vue'
 </script>
 
 <template>
-  <Drawer />
+  <!-- <Drawer /> -->
   <div class="bg-white w-4/5 mx-auto rounded-xl shadow-xl mt-14">
     <Header />
 
     <div class="p-10">
-      <h2 class="text-3xl font-bold mb-10">Все кроссовки</h2>
+      <div class="flex justify-between items-center">
+        <h2 class="text-3xl font-bold mb-10">Все кроссовки</h2>
+
+        <div class="flex gap-4">
+          <select class="py-2 px-3 border rounded-md outline-none">
+            <option value="">По названию</option>
+            <option value="">По цене (дешевле)</option>
+            <option value="">По цене (дороже)</option>
+          </select>
+
+          <div class="relative">
+            <img class="absolute left-4 top-3" src="/search.svg" alt="" />
+            <input
+              class="border rounded-md py-2 pl-11 pr-4 outline-none focus:border-gray-400"
+              placeholder="Поиск..."
+            />
+          </div>
+        </div>
+      </div>
 
       <CardList />
     </div>
